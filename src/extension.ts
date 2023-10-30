c562c464b131d877df644e30fdc8c11b4438e6a7
Sebastian Wiesner <sebastian@swsnr.de>
2023-10-30 22:59:48 +0100

    Remove unused import

--- src/extension.ts
+++ src/extension.ts
@@ -21,10 +21,7 @@ import Gio from "gi://Gio";
 import Soup from "gi://Soup";
 import GLib from "gi://GLib";
 
-import {
-  Extension,
-  gettext as _,
-} from "resource:///org/gnome/shell/extensions/extension.js";
+import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
 import * as Main from "resource:///org/gnome/shell/ui/main.js";
 
 import { DownloadScheduler } from "./lib/download.js";
