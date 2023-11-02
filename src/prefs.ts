af8a0da35a544ddd437c8e845e2f2f8e5a6ec265
Sebastian Wiesner <sebastian@swsnr.de>
2023-11-02 22:10:17 +0100

    Add page for source settings
    Currently we can only configure the APOD API key here.

--- src/prefs.ts
+++ src/prefs.ts
@@ -23,10 +23,11 @@ import Gtk from "gi://Gtk";
 import Adw from "gi://Adw";
 
 import {
-  gettext as _,
   ExtensionPreferences,
   ExtensionMetadata,
 } from "resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js";
+
+import apod from "./lib/sources/metadata/apod.js";
 
 const LICENSE = `Copyright Sebastian Wiesner <sebastian@swsnr.de>
 
@@ -47,21 +48,80 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.`;
 
+const getTemplateUri = (templateDirectory: Gio.File, name: string): string => {
+  const template = templateDirectory.get_child(`${name}.ui`).get_uri();
+  if (template === null) {
+    throw new Error(
+      `Failed to get URI for template ${name}.ui in ${templateDirectory.get_path()}`,
+    );
+  }
+  return template;
+};
+
+interface SourcesPageChildren {
+  readonly _apodGroup: Adw.PreferencesGroup;
+  readonly _apodApiKey: Adw.EntryRow;
+}
+
 interface AboutPageChildren {
-  _extensionName: Gtk.Label;
-  _extensionDescription: Gtk.Label;
-  _linkGithub: Gtk.LinkButton;
-  _linkIssues: Gtk.LinkButton;
-  _extensionLicense: Gtk.TextView;
+  readonly _extensionName: Gtk.Label;
+  readonly _extensionDescription: Gtk.Label;
+  readonly _linkGithub: Gtk.LinkButton;
+  readonly _linkIssues: Gtk.LinkButton;
+  readonly _extensionLicense: Gtk.TextView;
+}
+
+interface WindowSettingRegistry {
+  _settings: Gio.Settings;
+  _sourceSettings: Map<string, Gio.Settings>;
 }
 
 export default class PictureOfTheDayPreferences extends ExtensionPreferences {
-  private loadPages(templateDirectory: Gio.File) {
+  /**
+   * Load all pages.
+   *
+   * @param templateDirectory The directory for UI templates
+   * @param settings A registry for settings
+   * @returns All page classes
+   */
+  private loadPages(
+    templateDirectory: Gio.File,
+    settings: WindowSettingRegistry,
+  ) {
+    const getSettings = (schema: string): Gio.Settings =>
+      this.getSettings(schema);
+
+    const SourcesPage = GObject.registerClass(
+      {
+        GTypeName: "SourcesPage",
+        Template: getTemplateUri(templateDirectory, "SourcesPage"),
+        InternalChildren: ["apodGroup", "apodApiKey"],
+      },
+      class SourcesPage extends Adw.PreferencesPage {
+        constructor() {
+          super();
+
+          const apodSettings = getSettings(
+            `${settings._settings.schema_id}.source.${apod.key}`,
+          );
+
+          const children = this as unknown as SourcesPageChildren;
+          children._apodGroup.title = apod.name;
+          children._apodGroup.description = `<a href="${apod.website}">${apod.website}</a>`;
+          apodSettings.bind(
+            "api-key",
+            children._apodApiKey,
+            "text",
+            Gio.SettingsBindFlags.DEFAULT,
+          );
+        }
+      },
+    );
+
     const AboutPage = GObject.registerClass(
       {
         GTypeName: "AboutPage",
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        Template: templateDirectory.get_child("AboutPage.ui").get_uri()!,
+        Template: getTemplateUri(templateDirectory, "AboutPage"),
         InternalChildren: [
           "extensionName",
           "extensionDescription",
@@ -70,13 +130,9 @@ export default class PictureOfTheDayPreferences extends ExtensionPreferences {
           "extensionLicense",
         ],
       },
-
       class AboutPage extends Adw.PreferencesPage {
         constructor(metadata: ExtensionMetadata) {
-          super({
-            title: _("About"),
-            icon_name: "dialog-information-symbolic",
-          });
+          super();
 
           // TODO: Find a better way to declare that an instance has a set of props
           const children = this as unknown as AboutPageChildren;
@@ -89,16 +145,29 @@ export default class PictureOfTheDayPreferences extends ExtensionPreferences {
       },
     );
 
-    return { AboutPage };
+    return { AboutPage, SourcesPage };
   }
 
   override fillPreferencesWindow(
-    window: Adw.PreferencesWindow & {
-      _settings: Gio.Settings;
-    },
+    window: Adw.PreferencesWindow & WindowSettingRegistry,
   ): void {
+    // Add our icons directory to the Gtk theme path, so that we're able to use
+    // our icons in Adwaita widgets.
+    const iconTheme = Gtk.IconTheme.get_for_display(window.get_display());
+    const iconsDirectory = this.metadata.dir.get_child("icons").get_path();
+    if (iconsDirectory === null) {
+      throw new Error("Failed to get path for icon directory");
+    }
+    iconTheme.add_search_path(iconsDirectory);
+
+    // Attach our settings to the window to keep them alive as long as the window lives
+    window._settings = this.getSettings();
+    window._sourceSettings = new Map();
+
+    // Load pages from templates and add them to the window.
     const uiDir = this.metadata.dir.get_child("ui");
-    const Pages = this.loadPages(uiDir);
+    const Pages = this.loadPages(uiDir, window);
+    window.add(new Pages.SourcesPage());
     window.add(new Pages.AboutPage(this.metadata));
   }
 }
