60a8d6b860810078d0971c88fe4357c308471a70
Sebastian Wiesner <sebastian@swsnr.de>
2024-11-07 21:09:43 +0100

    Use shared eslint configuration

--- eslint.config.dist.js
+++ eslint.config.dist.js
@@ -4,28 +4,8 @@
 // License, v. 2.0.If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-import tseslint from "typescript-eslint";
+// @ts-check
 
-export default tseslint.config({
-  extends: [],
-  languageOptions: {
-    parserOptions: {
-      ecmaVersion: 2022,
-      sourceType: "module",
-    },
-  },
-  linterOptions: {
-    noInlineConfig: true,
-  },
-  rules: {
-    "lines-between-class-members": ["error", "always"],
-    "padding-line-between-statements": [
-      "error",
-      { blankLine: "always", prev: "*", next: "class" },
-      { blankLine: "always", prev: "*", next: "function" },
-      { blankLine: "always", prev: "*", next: "return" },
-      { blankLine: "always", prev: "*", next: "export" },
-      { blankLine: "always", prev: "*", next: "multiline-const" },
-    ],
-  },
-});
+import { dist } from "@swsnr/gsebuild/eslint";
+
+export default dist;
--- eslint.config.js
+++ eslint.config.js
@@ -6,23 +6,12 @@
 
 // @ts-check
 
-import eslint from "@eslint/js";
+import gsebuild from "@swsnr/gsebuild/eslint";
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
-import pluginPromise from "eslint-plugin-promise";
-
-import gjsGuide from "./eslint.config.gjs-guide.js";
-
-// Consider eslint-plugin-promise again once it supports flat config,
-// see https://github.com/eslint-community/eslint-plugin-promise/issues/449
 
 export default tseslint.config(
-  // See https://typescript-eslint.io/getting-started
-  eslint.configs.recommended,
-  ...gjsGuide,
-  ...tseslint.configs.strictTypeChecked,
-  ...tseslint.configs.stylisticTypeChecked,
-  // See https://typescript-eslint.io/getting-started/typed-linting
+  ...gsebuild,
   {
     languageOptions: {
       parserOptions: {
@@ -32,20 +21,8 @@ export default tseslint.config(
       },
     },
   },
-  // See https://github.com/eslint-community/eslint-plugin-promise?tab=readme-ov-file#usage
-  pluginPromise.configs["flat/recommended"],
   // See https://github.com/prettier/eslint-config-prettier?tab=readme-ov-file#installation
   eslintConfigPrettier,
-  {
-    linterOptions: {
-      reportUnusedDisableDirectives: "error",
-    },
-    rules: {
-      // .eslintrc.gjs-guide.yml enables this, but it has no use in typescript
-      // which ensures a consistent return value through its type checks.
-      "consistent-return": "off",
-    },
-  },
   // Global ignores, see https://eslint.org/docs/latest/use/configure/configuration-files#globally-ignoring-files-with-ignores
   // "ignores" must be the _only_ key in this object!
   {
